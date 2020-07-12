f739c7f8cbc625e7605dc4c8adc2d0c3d80be55d
Jackson <jacksonludwig0@gmail.com>
2020-07-12 13:27:18 -0400

    init

---
+++ src/host.rs
@@ -0,0 +1,34 @@
+use std::io::prelude::*;
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use threadpool::ThreadPool;
+
+use std::str;
+const BUFFER_SIZE: usize = 4096;
+
+pub fn start_hosting(num_workers: usize, listener_ip: &str) -> std::io::Result<()> {
+    println!("Server started on: {}", listener_ip);
+    create_pool(num_workers, listener_ip)?;
+    Ok(())
+}
+
+fn create_pool(num_workers: usize, listener_ip: &str) -> std::io::Result<()> {
+    let listener = TcpListener::bind(listener_ip)?;
+    let pool = ThreadPool::new(num_workers);
+    loop {
+        let (stream, address) = listener.accept()?;
+        pool.execute(move || {
+            handle_connection(stream, address);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream, address: SocketAddr) {
+    println!("Received connection from: {}", address.to_string());
+    let mut buffer = vec![0; BUFFER_SIZE];
+    let bytes_read = stream.read(&mut buffer).unwrap();
+    println!("bytes read: {}", bytes_read);
+    println!(
+        "Buffer currently holds: {:?}",
+        str::from_utf8(&buffer[0..bytes_read]).unwrap()
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::path::Path;
+use std::result::Result;
+
+mod host;
+
+fn main() -> std::io::Result<()> {
+    match host_or_client().unwrap() {
+        RunType::Host => host::start_hosting(4, "127.0.0.1:7878")?,
+        RunType::Client => {
+            let stream = TcpStream::connect("127.0.0.1:7878")?;
+            if let Err(e) = send_file(stream, "C:\\Users\\jacks\\Desktop\\test.txt") {
+                panic!("The file was not able to be sent: {:?}", e);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+enum RunType {
+    Host,
+    Client,
+}
+
+fn host_or_client<'a>() -> Result<RunType, &'a str> {
+    println!("1 - Client\n2 - Host");
+    let choice: u8;
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Failed to read line");
+
+    choice = match buffer.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return Err("Only enter integer responses"),
+    };
+
+    match choice {
+        1 => Ok(RunType::Client),
+        2 => Ok(RunType::Host),
+        _ => Err("You must be either a host or client to transfer files"),
+    }
+}
+
+fn send_file(mut stream: TcpStream, path: &str) -> io::Result<()> {
+    let path = Path::new(path);
+    let file_name = path.file_name().unwrap();
+    println!("File name: {:?}", file_name);
+
+    let mut file = File::open(path)?;
+    let file_size = file.metadata().unwrap().len();
+    println!("File size: {}", file_size);
+
+    let mut buffer = vec![0; file_size as usize];
+    let read_amt = file.read(&mut buffer)?;
+    println!("Bytes read from file: {}", read_amt);
+
+    let written_amt = stream.write(&buffer)?;
+    println!("Bytes written to stream: {}", written_amt);
+    Ok(())
+}
