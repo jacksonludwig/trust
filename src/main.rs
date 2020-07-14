b5b3c10431c3a8fd0d9f18e6a7fec79a596dd28a
Jackson <jacksonludwig0@gmail.com>
2020-07-14 12:02:44 -0400

    change host

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod host;
 use handler::RunType;
 
 const NUM_THREADS: usize = 4;
-const SERVER_IP: &str = "127.0.0.1:7878";
+const SERVER_IP: &str = "0.0.0.0:7878";
 
 fn main() -> std::io::Result<()> {
     match handler::host_or_client().unwrap() {
